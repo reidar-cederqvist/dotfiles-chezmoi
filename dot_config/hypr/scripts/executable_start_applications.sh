b21ec58a81ed43e2e370ab06995f8b24fffc3abc
Reidar Cederqvist <reidar.cederqvist@protonmail.com>
2024-03-22 10:30:27 +0100

    Update .config/dunst/dunstrc Update .config/hypr/scripts/start_applications.sh

--- dot_config/hypr/scripts/executable_start_applications.sh
+++ dot_config/hypr/scripts/executable_start_applications.sh
@@ -4,7 +4,7 @@
 waybar &
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 "$SCRIPT_DIR"/test.py
-mako &
+dunst &
 wl-paste --watch cliphist store &
 hyprpaper &
 pasystray &
