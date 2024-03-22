d86b84279a0778797a2ffb8c08bf40277b5debb8
Reidar Cederqvist <reidar.cederqvist@protonmail.com>
2024-03-22 10:23:33 +0100

    fix the path to test.py

--- dot_config/hypr/scripts/executable_start_applications.sh
+++ dot_config/hypr/scripts/executable_start_applications.sh
@@ -2,8 +2,8 @@
 #
 
 waybar &
-
-$HOME/test.py
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+"$SCRIPT_DIR"/test.py
 mako &
 wl-paste --watch cliphist store &
 hyprpaper &
